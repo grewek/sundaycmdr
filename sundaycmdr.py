d4a6e5203ffb57433a7eb5beb008c867d9e79d50
Kay Grewe <Kay.Grewe@protonmail.com>
2023-04-14 16:05:26 +0200

    Refactor of the View class, added scrolling for the view class

--- sundaycmdr.py
+++ sundaycmdr.py
@@ -5,37 +5,56 @@ import curses
 from curses import wrapper
 
 class View:
-    
-    def __init__(self, fileList, win, height):
+    def __init__(self, fileList, width, height, startY=0, startX=0):
         self.height = height
+        self.width = width
         self.selectionIndex = 0
         self.fileList = fileList 
-        self.window = win
+        #TODO: Test the Pad Window type from the curses lib, it seems to be a scrolling content window as well
+        #      but with a very clunky API...
+        self.window = curses.newwin(height, width, startY, startX) 
+        
+        self.minIndex = 0
+        self.maxIndex = self.height
+        
+        self.startY = startY
+        self.startX = startX
 
-    def handle_command(self, pressed_key):
+    def ValidateSelectionIndex(self):
+        if self.selectionIndex - 1 < 0:
+            self.selectionIndex = 0
+
+        distanceToEnd = len(self.fileList) - self.selectionIndex
+        if self.selectionIndex > self.height and distanceToEnd <= 0:
+            self.selectionIndex = len(self.fileList) - 1
+
+            
+    def HandleKeyEvents(self, pressed_key):
         if pressed_key == 'j':
-            if self.selectionIndex + 1 >= self.height:
-                self.selctionIndex = self.height
-            else:
-                self.selectionIndex += 1
+            self.selectionIndex += 1
         elif pressed_key == 'k':
-            if self.selectionIndex - 1 < 0:
-                self.selctionIndex = 0
-            else:
-                self.selectionIndex -= 1  
+            self.selectionIndex -= 1  
 
-    def update(self):
-        for i, file in enumerate(self.fileList[0:self.height]):
-            if i == self.selectionIndex:
+    def CalculateFileSliceRange(self):
+        #NOTE: Change the divisior in the minIndex to get a different turnaround point
+        self.minIndex = max(0, self.selectionIndex - int(self.height/2)) 
+        self.maxIndex = self.minIndex + self.height
+        
+    def Update(self):
+        self.window.clear()
+        self.ValidateSelectionIndex()
+        self.CalculateFileSliceRange()
+
+        for i, file in enumerate(self.fileList[self.minIndex:self.maxIndex]):
+            if i + self.minIndex == self.selectionIndex:
                 curses.start_color()
-                self.window.addstr(i, 0, file, curses.color_pair(curses.COLOR_RED))
+                self.window.addstr(i, 0, "{} {}".format(i + self.minIndex, file), curses.color_pair(curses.COLOR_RED))
             else:
-                self.window.addstr(i, 0, file)       
+                self.window.addstr(i, 0, "{} {}".format(i + self.minIndex, file))       
 
         return True
 
-    def draw(self, stdscr):
-        stdscr.refresh()
+    def Draw(self):
         self.window.refresh()
     
 def ReadAllFilesInHomeDirectory():
@@ -54,26 +73,25 @@ def main(stdscr):
     splitRatio = .5
     viewWidth = int(winWidth * splitRatio)
 
-    leftView = View(ReadAllFilesInHomeDirectory(), curses.newwin(winHeight, viewWidth, 0, 0), winHeight)
-    rightView = View(ReadAllFilesInHomeDirectory(), curses.newwin(winHeight, viewWidth, 0, viewWidth), winHeight)
-
-    views = [View(ReadAllFilesInHomeDirectory(), curses.newwin(winHeight, viewWidth, 0, 0), winHeight),
-             View(ReadAllFilesInHomeDirectory(), curses.newwin(winHeight, viewWidth, 0, viewWidth), winHeight)]
+    views = [View(ReadAllFilesInHomeDirectory(), viewWidth, winHeight),
+             View(ReadAllFilesInHomeDirectory(), viewWidth, winHeight, 0, viewWidth)]
     selectedView = 0
     
     #curses, incantations, song and dance...for colors
+    #jokes aside this tells curses it should use the terminal background color and a red foreground color
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_RED, -1)
     
     while(True):
-        views[0].update()
-        views[1].update()
+        views[0].Update()
+        views[1].Update()
 
-        views[0].draw(stdscr)
-        views[1].draw(stdscr)
+        stdscr.refresh()
+        views[0].Draw()
+        views[1].Draw()
         
         pressed_key = stdscr.getkey()
-        views[selectedView].handle_command(pressed_key)
+        views[selectedView].HandleKeyEvents(pressed_key)
 
         if pressed_key == 'q':
             return
